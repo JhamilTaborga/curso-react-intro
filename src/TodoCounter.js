a079c5e9d3ff0c0a0568f17dc766ddf736d29ea9
Jhamil Taborga <jhamiltaborga@gmail.com>
2024-09-10 16:40:13 -0400

    Agregué una función para que cuando se completen los Todos te felicite

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,11 +1,20 @@
 import './TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
-    return (
-      <h1 className='TodoCounter'>
-        Has completado <span>{completed}</span> tareas de <span>{total}</span>
-      </h1>
-    );
+    if ( total == completed) {
+      return (
+        <h1 className='TodoCounter'>
+          Felicidades, has terminado todos los TODOS!🥳
+        </h1>
+      );
+    } else {
+      return (
+        <h1 className='TodoCounter'>
+          Has completado <span>{completed}</span> tareas de <span>{total}</span>
+        </h1>
+      );
+    } 
+  
 }
 
 export { TodoCounter };
