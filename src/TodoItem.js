b3f7480ce7aae921d615537be148608fd46a1583
Jhamil Taborga <jhamiltaborga@gmail.com>
2024-09-10 20:16:23 -0400

    Agregamos los iconos con TodoIcon.js y la funcionalidad con las props, para esto agregamos elementos svg

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,17 @@
+import { CompleteIcon } from './CompleteIcon'
+import { DeleteIcon } from './DeleteIcon'
 import './TodoItem.css';
 
 function TodoItem(props) {
     return (
       <li className='TodoItem'>
-        <button className='Button Button-check' onClick={props.onComplete}> </button>
+        <CompleteIcon />
+        {/* <button className='Button Button-check' onClick={props.onComplete}> </button> */}
         <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
           {props.text}
         </p>
-        <button className='Button Button-delete' onClick={props.onDelete}> </button>
+        {/* <button className='Button Button-delete' onClick={props.onDelete}> </button> */}
+        <DeleteIcon />
       </li>
     );
 };
