9c8a6c6ced06c9807b14c90e61738c47059b051e
Jhamil Taborga <jhamiltaborga@gmail.com>
2024-09-13 14:07:45 -0400

    Actualicé la felicitación, mejoré la lógica del TodoCounter y le agregué algo de diseño

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,20 +1,30 @@
 import './TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
-    if ( total == completed) {
-      return (
+  if (total == 0) {
+    return ( 
+      <h1 className='TodoCounter'>
+        Aún no tienes TODOS Agregados
+      </h1>
+    );
+  } else if (total == completed) {
+    return (
+      <>
         <h1 className='TodoCounter'>
+        Has completado <span>{completed}</span> tareas de <span>{total}</span>
+        </h1>
+        <h2 className='TodoCounterCongratulate'>
           Felicidades, has terminado todos los TODOS!🥳
-        </h1>
-      );
-    } else {
-      return (
-        <h1 className='TodoCounter'>
-          Has completado <span>{completed}</span> tareas de <span>{total}</span>
-        </h1>
-      );
-    } 
-  
+        </h2>
+      </>
+    );
+  } else {
+    return (
+      <h1 className='TodoCounter'>
+        Has completado <span>{completed}</span> tareas de <span>{total}</span>
+      </h1>
+    );
+  } 
 }
 
 export { TodoCounter };
