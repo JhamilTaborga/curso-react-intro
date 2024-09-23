5d5308c49679446a1de9248e519c23d3b7fec33b
Jhamil Taborga <jhamiltaborga@gmail.com>
2024-09-23 13:47:24 -0400

    Reto: agregar funcionalidad del createTodoButton

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -1,14 +1,13 @@
 import './CreateTodoButton.css';
 
-function CreateTodoButton() {
+function CreateTodoButton({ setOpenModal }) {
     return (
         <button 
             className='CreateTodoTutton'
             onClick={
-                (event) => {
-                    console.log('le diste click');
-                    console.log(event);
-                    console.log(event.target)
+                () => {
+                    //Esta es otra forma para actualizar el estado. Con esta función recibimos el estado anterior y podemos actualizar el estado con esta función, en este caso tenemos en el estado inicial un booleano, si el estado inicial es "false" lo devolvemos como "true" y viceversa y así sucesivamente cada que se haga click al button.
+                    setOpenModal(state => !state);
                 }
             }
         > + </button>
